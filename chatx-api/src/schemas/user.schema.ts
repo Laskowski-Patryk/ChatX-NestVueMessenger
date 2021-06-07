ce4ef7865d569333989649f0f4f33b17c12c6911
Patryk Laskowski <lasekl1998@gmail.com>
2021-06-07 23:14:22 +0200

    x

--- chatx-api/src/schemas/user.schema.ts
+++ chatx-api/src/schemas/user.schema.ts
@@ -1,15 +1,17 @@
 import * as mongoose from 'mongoose';
 import { IUser } from '../interfaces/user.interface';
-export const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  login: { type: String, required: true },
-  password: { type: String, required: true },
-  emai_verified: Boolean,
-  name: { type: String, required: true },
-  surname: { type: String, required: true },
-  city: { type: String, required: true },
-  avatar: String
-});
+export const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    login: { type: String, required: true },
+    password: { type: String, required: true },
+    emai_verified: Boolean,
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    city: { type: String, required: true },
+    avatar: String,
+  },
+  { versionKey: false },
+);
 export const User = mongoose.model<IUser>('User', UserSchema);
 export default User;
-
