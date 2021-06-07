091880a3cc010b3d2e4af2800a4b2afd62fb6d87
Patryk Laskowski <lasekl1998@gmail.com>
2021-06-07 19:58:53 +0200

    z serwerem

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-const myV3App = createApp(App)
-myV3App
-.use(router)
-myV3App.mount('#app')
+const myV3App = createApp(App);
+myV3App.use(router);
+myV3App.mount("#app");
