f2f8dfcc5eeb2a560b06dd1063702a9ab71d596f
Patryk Laskowski <lasekl1998@gmail.com>
2021-06-07 20:05:45 +0200

    dodanie serwera

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="register">
     <div class="center">
-      <h1>Zarejestruj się</h1><br/>
+      <h1>Zarejestruj się</h1>
+      <h3>lub <router-link to="/signin">Zaloguj</router-link></h3> <!-- SCROLLBAR TODO -->
       <form>
       <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" icon="envelope" placeholder="Enter email">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label >Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Podaj Login">
+  </div>
+  <div class="form-group">
+    <label >Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Podaj Hasło">
+  </div>
+  <div class="form-group">
+    <label >Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Podaj Imie">
+  </div>
+  <div class="form-group">
+    <label >Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Podaj Nazwisko">
   </div>
   <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model="user.terms">
     <label class="form-check-label" for="exampleCheck1">Akceptuję regulamin serwisu</label>
   </div>
-  <button type="submit" class="btn btn-primary" >Zarejestruj się</button>
+  <button type="submit">Zarejestruj się</button>
   </form>
     </div>
   </div>
@@ -33,7 +46,8 @@ export default {
         surname: "",
         city: "",
         email: "",
-        password: ""
+        password: "",
+        terms: false,
       }
     };
   },
@@ -49,13 +63,57 @@ export default {
 .center {
   position: absolute;
   margin: auto;
-  left: 35%;
-  top: 10%;
-  width: 30%;
-  height: 80%;
+  left: clamp(2rem, 40%, 35%);
+  top: 8%;
+  width: clamp(10%, 35%, 30%);
+  height: 86%;
   border-radius: 2vh;
   box-shadow: 10px 10px 20px #bababa, -10px -10px 20px #ffffff;
   box-sizing: border-box;
-  padding: 3%;
+  padding: clamp(2rem, 3%, 35%);
+  overflow: auto;
+  padding-top:2rem !important;
 }
+
+button{
+  margin: 25px 0 0 0;
+  width: 100%;
+  height: 50px;
+  color: #000;
+  font-size: 18px;
+  font-weight: 600;
+  background: #dde1e7;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 25px;
+  opacity: 0.8;
+  box-shadow: 2px 2px 5px #bababa,
+              -5px -5px 10px #ffffff;
+}
+
+button:focus{
+  color: #3498db;
+  box-shadow: inset 2px 2px 5px #bababa,
+              inset -5px -5px 10px #ffffff;
+}
+.form-control{
+  height:3rem !important;
+  height: 100%;
+  width: 100%;
+  padding-left: 45px;
+  font-size: 18px;
+  outline: none;
+  border: none;
+  color: #595959;
+  background: #dde1e7;
+  border-radius: 25px;
+  box-shadow: inset 2px 2px 5px #babecc,
+              inset -5px -5px 10px #ffffff;
+}
+.form-control input:focus ~ label{
+  box-shadow: inset 2px 2px 5px #babecc,
+              inset -1px -1px 2px #ffffff;
+}
+
 </style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    
+  <div class="about"> 
+      Hewllo
   </div>
 </template>
 <script>
