2de7bb137fdfcc56bd87bb339bacda53684ed1dd
Patryk Laskowski <lasekl1998@gmail.com>
2021-06-11 23:09:38 +0200

    kontrolery moduły services

--- chatx-api/src/users/users.module.ts
+++ chatx-api/src/users/users.module.ts
@@ -6,11 +6,10 @@ import { UsersService } from './users.service';
 import { UserSchema } from '../schemas/user.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
-  ],
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService], 
-  exports: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
+ 
